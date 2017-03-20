79ac2f20dc7a578737657566695368f4adf535dc
Alexey Milyukov <alexeymilyukov@Alexeys-MacBook-Pro.local>
2017-03-19 19:27:29 -0700

    Fixing frontend submition

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,9 @@
 class Order < ApplicationRecord
   validates :name,  :presence => true,
-            :length => {:minimum => 2, :maximum => 100}
+            :length => {:minimum => 1, :maximum => 100}
 
   validates :email,  :presence => true,
-    :length => {:minimum => 2, :maximum => 100}
+    :length => {:minimum => 1, :maximum => 100}
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'orders/new'
+  post 'orders/create'
 
   post 'orders/update'
 
