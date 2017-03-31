5382b8c436d6fa1307587305359447ca2a0f47e8
alexey <amilyukov@amilyukov.local>
2017-03-30 18:47:56 -0700

    Changing logos

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20170319092741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "applications", force: :cascade do |t|
-    t.string   "email"
-    t.string   "name"
-    t.text     "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
