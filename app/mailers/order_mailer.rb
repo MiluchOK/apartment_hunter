7a896d365dd7af7b0e7efc3bc1d757e59a4e2a1b
Alexey Milyukov <milyukov.alexey@gmail.com>
2017-04-18 00:49:52 -0700

    Adding mail base

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,6 @@
+class OrderMailer < ApplicationMailer
+  def sample_email
+    # mail(to: 'milyukov.alexey@gmail.com', subject: 'Sample Email')
+    mail(to: 'phycomycetes@gmail.com', subject: 'Sample Email')
+  end
+end
